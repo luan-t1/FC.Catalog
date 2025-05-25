a2937b809de94cdec795ddad515d53d59ab20301
Luan Tavares <112911703+luan-t1@users.noreply.github.com>
2025-05-25 19:15:13 -0300

    feat: creating repository

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,49 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using Moq;
+using UseCases = FC.Codeflix.Catalog.Application.UseCases.CreateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+
+public class CreateCategoryTest
+{
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategory()
+    {
+        var repositoryMock = new Mock<ICategoryRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var useCase = new UseCases.CreateCategory(
+            repositoryMock.Object,
+            unitOfWorkMock.Object);
+
+        var input = new CreateCategoryInput(
+            "category Name",
+            "Category Description",
+            true
+        );
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(repository => repository.Insert(
+            It.IsAny<Category>(),
+            It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+
+        unitOfWorkMock.Verify(uow => uow.Commit(
+            It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+
+        output.shouldnotBeNull();
+        output.Name.Should().Be("category Name");
+        output.Description.Should().Be("Category Description");
+        output.IsActive.Should().Be(true);
+        (output.Id != null && output.Id != Guid.Empty).Should().BeTrue();
+        (output.CreatedAt != null && output.Id != default(DateTime)).Should().BeTrue();
+    }
+}
