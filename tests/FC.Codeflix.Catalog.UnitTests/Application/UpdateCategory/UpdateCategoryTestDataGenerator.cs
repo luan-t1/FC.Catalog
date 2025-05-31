c7164d89e1420104f911658b9c2f095c5b63f4fe
Luan Tavares <112911703+luan-t1@users.noreply.github.com>
2025-05-30 22:19:01 -0300

    creating update category use case and test

--- tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestDataGenerator.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
 
@@ -10,18 +11,51 @@ public class UpdateCategoryTestDataGenerator
         for (int i = 0; i < times; i++)
         {
             var exampleCategory = fixture.GetExampleCategory();
-            var exampleInput = new UpdateCategoryInput(
-                exampleCategory.Id,
-                fixture.GetValidCategoryName(),
-                fixture.GetValidCategoryDescription(),
-                fixture.getRandomBoolean()
-                );
+            var exampleInput = fixture.GetValidInput(exampleCategory.Id);
 
             yield return new object[]
             {
                 exampleCategory, exampleInput
             };
         }
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputs(int times = 12)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+
+        var invalidInputsList = new List<object[]>();
+        var totalInvalidCases = 3;
+
+        for (int index = 0; index < times; index++)
+        {
+            switch (index % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputsList.Add(new object[]
+                    {
+                        fixture.GetInvalidInputShortName(),
+                        "Name should be at least 3 characters long"
+                    });
+                    break;
+                case 1:
+                    invalidInputsList.Add(new object[]
+                    {
+                        fixture.GetInvalidInputTooLongName(),
+                        "Name should be less or equal to 255 characters long"
+                    });
+                    break;
+                case 2:
+                    invalidInputsList.Add(new object[]
+                    {
+                        fixture.GetInvalidInputTooLongDescription(),
+                        "Description should be less or equal to 10000 characters long"
+                    });
+                    break;
+            }
+        }
+
+        return invalidInputsList;
     }
 }
 
--- tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using FC.Codeflix.Catalog.Application.Interfaces;
 using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
 using FC.Codeflix.Catalog.Domain.Entity;
 using FC.Codeflix.Catalog.Domain.Repository;
 using FC.Codeflix.Catalog.UnitTests.Common;
@@ -49,4 +50,44 @@ public class UpdateCategoryTestFixture : BaseFixture
                         GetValidCategoryDescription(),
                         getRandomBoolean()
                    );
+
+    public UpdateCategoryInput GetValidInput(Guid? id = null)
+        => new (
+                id ?? Guid.NewGuid(),
+                GetValidCategoryName(),
+                GetValidCategoryDescription(),
+                getRandomBoolean()
+                );
+
+    public UpdateCategoryInput GetInvalidInputShortName()
+    {
+        var invalidInputShortName = GetValidInput();
+        invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+        return invalidInputShortName;
+    }
+
+    public UpdateCategoryInput GetInvalidInputTooLongName()
+    {
+        var invalidInputTooLongName = GetValidInput();
+        var tooLongNameForCategory = Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+        {
+            tooLongNameForCategory = $"{tooLongNameForCategory}{Faker.Commerce.ProductName()}";
+        }
+        invalidInputTooLongName.Name = tooLongNameForCategory;
+
+        return invalidInputTooLongName;
+    }
+
+    public UpdateCategoryInput GetInvalidInputTooLongDescription()
+    {
+        var invalidInputTooLongDescription = GetValidInput();
+        var tooLongDescriptionForCategory = Faker.Commerce.ProductDescription();
+        while (tooLongDescriptionForCategory.Length <= 10_000)
+        {
+            tooLongDescriptionForCategory = $"{tooLongDescriptionForCategory}{Faker.Commerce.ProductDescription()}";
+        }
+        invalidInputTooLongDescription.Description = tooLongDescriptionForCategory;
+        return invalidInputTooLongDescription;
+    }
 }
