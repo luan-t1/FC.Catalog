1faae493cf61516e956583b72c6e5b82741d352b
Luan Tavares <112911703+luan-t1@users.noreply.github.com>
2025-05-31 15:44:35 -0300

    finishing update category

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,28 @@
+﻿namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ListCategories))]
+    [Trait("Application", "ListCategories - Use Cases")]
+    public async Task ListCategories()
+    {
+        // Arrange
+        var useCase = _fixture.GetListCategoriesUseCase();
+        var input = _fixture.GetListCategoriesInput();
+
+        // Act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        // Assert
+        _fixture.AssertListCategoriesOutput(output);
+    }
+
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture>
+{
+}
+
+public class ListCategoriesTestFixture : BaseFixture
+{
+}
