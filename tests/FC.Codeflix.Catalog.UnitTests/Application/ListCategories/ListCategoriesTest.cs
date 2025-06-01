27d421011757728e6cd5bd7c3063c8b6cb3a35cd
Luan Tavares <112911703+luan-t1@users.noreply.github.com>
2025-06-01 14:41:44 -0300

    creating ListCategories initial tests

--- tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -1,4 +1,7 @@
-﻿namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
 
 [Collection(nameof(ListCategoriesTestFixture))]
 public class ListCategoriesTest
@@ -10,19 +13,40 @@ public class ListCategoriesTest
         _fixture = fixture;
     }
 
-    [Fact(DisplayName = nameof(ListCategories))]
+    [Fact(DisplayName = nameof(List))]
     [Trait("Application", "ListCategories - Use Cases")]
-    public async Task ListCategories()
+    public async Task List()
     {
-        // Arrange
-        var useCase = _fixture.GetListCategoriesUseCase();
-        var input = _fixture.GetListCategoriesInput();
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = new ListCategoriesInput(
+            page: 2,
+            perPage: 15,
+            search: "search-example",
+            sort: "name",
+            dir: SearchOrder.Asc
+        );
+        repositoryMock.Setup(x => x.Search(
+            It.IsAny<SearchInput>(
+                searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir),
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(new OutputSearch<Category>(
+                currentPage: input.Page,
+                perPage: input.PerPage,
+                items: (IReadOnlyList<Category>) categoriesExampleList,
+                total: 70
+            ));
+        var useCase = new ListCategories(repositoryMock.Object);
 
-        // Act
         var output = await useCase.Handle(input, CancellationToken.None);
 
-        // Assert
-        _fixture.AssertListCategoriesOutput(output);
+        output.Should().NotBeNull();
+        output.
+
+
     }
 
 }
--- tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -1,4 +1,8 @@
-﻿using FC.Codeflix.Catalog.UnitTests.Common;
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using Moq;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
 
@@ -9,4 +13,50 @@ public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCatego
 
 public class ListCategoriesTestFixture : BaseFixture
 {
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName[..255];
+        }
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+        {
+            categoryDescription = categoryDescription[..10_000];
+        }
+        return categoryDescription;
+    }
+
+    public bool getRandomBoolean()
+       => (new Random()).NextDouble() < 0.5;
+
+    public Category GetExampleCategory()
+        => new Category(
+                        GetValidCategoryName(),
+                        GetValidCategoryDescription(),
+                        getRandomBoolean()
+                   );
+
+    public List<Category> GetExampleCategoriesList(int length = 10)
+    {
+        var list = new List<Category>();
+        for (int i = 0; i < length; i++)
+        {
+            list.Add(GetExampleCategory());
+        }
+        return list;
+    }
 }
