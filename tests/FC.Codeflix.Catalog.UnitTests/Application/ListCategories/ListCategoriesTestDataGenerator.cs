3fd1d84afb3721e848a5913f6cfe67e834df4dd2
Luan Tavares <112911703+luan-t1@users.noreply.github.com>
2025-06-04 23:13:11 -0300

    refactoring listCategoriesTest

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestDataGenerator.cs
@@ -0,0 +1,35 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+public class ListCategoriesTestDataGenerator
+{
+    public static IEnumerable<object[]> GetInputsWithoutAllParameter(int times = 10)
+    {
+        var fixture = new ListCategoriesTestFixture();
+
+        var inputExample = fixture.GetExampleInput();
+        for(int i = 0;i < times; i++)
+        {
+            switch (i % 5)
+            {
+                case 0:
+                    yield return new object[]{ ListCategoriesInput() }; 
+                    break;
+                case 1:
+                    yield return new object[] { ListCategoriesInput(inputExample.Page) };
+                    break;
+                case 2:
+                    yield return new object[] { ListCategoriesInput(inputExample.Page,inputExample.PerPage) };
+                    break;
+                case 3:
+                    yield return new object[] { ListCategoriesInput(inputExample.Page, inputExample.PerPage, inputExample.Search) };
+                    break;
+                case 4:
+                    yield return new object[] { ListCategoriesInput(inputExample.Page, inputExample.PerPage, inputExample.Search, inputExample.Sort) };
+                    break;
+                default:
+            }
+        }
+    }
+}
--- tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -1,6 +1,8 @@
 ﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
 using FC.Codeflix.Catalog.Domain.Entity;
 using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 using FC.Codeflix.Catalog.UnitTests.Common;
 using Moq;
 
@@ -58,5 +60,17 @@ public class ListCategoriesTestFixture : BaseFixture
             list.Add(GetExampleCategory());
         }
         return list;
+    }
+
+    public ListCategoriesInput GetExampleInput()
+    {
+        var random = new Random();
+        return new ListCategoriesInput(
+            page: random.Next(1,10),
+            perPage: random.Next(15, 100),
+            search: Faker.Commerce.ProductName(),
+            sort: Faker.Commerce.ProductName(),
+            dir: random.Next(0,10) > 5 ? SearchOrder.Asc : SearchOrder.Desc
+        );
     }
 }
