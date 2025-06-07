0c0b7693bf7ef6a2d90fc056936a04337c8db43a
Luan Tavares <112911703+luan-t1@users.noreply.github.com>
2025-06-06 22:04:12 -0300

    creating update integration test

--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -74,4 +74,26 @@ public class CategoryRepositoryTest
         task.Should().ThrowAsync<NotFoundException>()
             .WithMessage($"Category '{exampleId}' not found.");
     }
+
+    [Fact(DisplayName = nameof(Update))]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task Update()
+    {
+        CodeflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleCategory = _fixture.GetExampleCategory();
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(15);
+        exampleCategoriesList.Add(exampleCategory);
+        await dbContext.AddRangeAsync(exampleCategoriesList);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+        var categoryRepository = new Repository.CategoryRepository(dbContext);
+
+        var dbCategory = await categoryRepository.Get(exampleCategory.Id, CancellationToken.None);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(exampleCategory.Name);
+        dbCategory.Id.Should().Be(exampleCategory.Id);
+        dbCategory.Description.Should().Be(exampleCategory.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+    }
 }
